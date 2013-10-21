69b8c0c867ea37a7b5a822943d7298666611adc7
Adrian-M <adrian.marotzke@tuhh.de>
2013-10-21 17:38:09 +0200

    Node.js web server now only accepts connections from localhost. Implemented rudimentary http method parsing (GET,PUT, etc.).

--- WebContent/node_js_server.js
+++ WebContent/node_js_server.js
@@ -6,10 +6,65 @@ var http = require("http"),
  
 http.createServer(function(request, response) {
  
-  console.log("request: " + request.url);
-  var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
+  console.log("request: " + request.method + " " + request.url);
+  var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri), methode = request.method;
+    
+	if(methode=="GET")
+	{
+		GET(filename,response);
+	}
+	else 
+	{ 
+		if (methode=="PUT") {
+			response.writeHead(500, {"Content-Type": "text/plain"});
+			response.write("Unsupported Request Methode\n");
+			response.end();
+			//PUT(filename,response,request);
+		}
+		else {
+			console.log("Unsupported Request: " + request.method + " " + request.url);
+			response.writeHead(500, {"Content-Type": "text/plain"});
+			response.write("Unsupported Request Methode\n");
+			response.end();
+			return;
+		}
+	}
+	
+}).listen(parseInt(port, 10),'localhost');
+ 
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+var toggle = true;
+function throttleUpload(req, msBusy, msWait) {
+  if(req.complete) 
+  {
+    toggle = true;
+    return;
+  }  
   
-    path.exists(filename, function(exists) {
+  if (toggle)
+  {
+    console.log("pause");
+    req.pause();
+    toggle = !toggle;
+    setTimeout(function() {
+      throttleUpload(req, msBusy, msWait);
+     }, msWait);
+   }
+   else
+   {
+    console.log("resume");
+    req.resume();
+    toggle = !toggle;
+    setTimeout(function() {
+      throttleUpload(req, msBusy, msWait);
+     }, msBusy);
+
+   }
+}
+
+function GET(filename,response) {
+	path.exists(filename, function(exists) {
 		
 		if(!exists) {
 			response.writeHead(404, {"Content-Type": "text/plain"});
@@ -45,35 +100,43 @@ http.createServer(function(request, response) {
 			}, 2000);*/
 		});
 	});
-}).listen(parseInt(port, 10));
- 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+}
 
-var toggle = true;
-function throttleUpload(req, msBusy, msWait) {
-  if(req.complete) 
-  {
-    toggle = true;
-    return;
-  }  
-  
-  if (toggle)
-  {
-    console.log("pause");
-    req.pause();
-    toggle = !toggle;
-    setTimeout(function() {
-      throttleUpload(req, msBusy, msWait);
-     }, msWait);
-   }
-   else
-   {
-    console.log("resume");
-    req.resume();
-    toggle = !toggle;
-    setTimeout(function() {
-      throttleUpload(req, msBusy, msWait);
-     }, msBusy);
-
-   }
+function PUT(filename,response) {
+	path.exists(filename, function(exists) {
+		
+		if(!exists) {
+			response.writeHead(404, {"Content-Type": "text/plain"});
+			response.write("404 Not Found\n");
+			response.end();
+			return;
+		}
+	 
+		if (fs.statSync(filename).isDirectory()) {
+			filename += '/index.html';
+		}
+	 
+		fs.readFile(filename, "binary", function(err, file) {
+		  if(err) {        
+			response.writeHead(500, {"Content-Type": "text/plain"});
+			response.write(err + "\n");
+			response.end();
+			return;
+		}
+	 
+		//throttleUpload(request, 5, 500)
+		
+		response.writeHead(200, {"Cache-Control": "no-cache, must-revalidate"});
+		
+		//response.writeHead(200, {"Cache-Control": "public, max-age=60, s-maxage=60"});
+		
+		
+		response.write(file, "binary");
+		
+		response.end();
+		/*setTimeout(function() {
+			response.end();
+			}, 2000);*/
+		});
+	});
 }
