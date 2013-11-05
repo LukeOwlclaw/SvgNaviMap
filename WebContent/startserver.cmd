b36122e5b776b622952821a6c10020108e70e005
Luke Owncloud <owncloud@ohrt.org>
2013-11-05 07:48:49 +0100

    check os version, add node-js 32 bit

--- WebContent/startserver.cmd
+++ WebContent/startserver.cmd
@@ -1,3 +1,23 @@
-start http://localhost:8888/editorsview.html
-node.exe node_js_server.js
+@echo off
+netstat -aon | find ":8888" > NUL
 
+IF %ERRORLEVEL% EQU 0 (
+  echo Port 8888 seems to be in use. Abort starting server.
+  goto end
+) ELSE (
+  echo Port 8888 is available. Continue...
+)
+
+
+IF %PROCESSOR_ARCHITECTURE%==x86 (
+    echo Starting node.js 32 bit...
+    start node-x86 node_js_server.js
+) ELSE (
+  echo Starting node.js 64 bit...
+    start node node_js_server.js
+)
+
+echo Opening Editor's View in default browser...
+start http://localhost:8888/editorsview.html
+
+:end
